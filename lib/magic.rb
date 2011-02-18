4640db73c21a52d58edc081a32fee11b41726097
Eric Monti <esmonti@gmail.com>
2011-02-17 19:18:23 -0600

    move convenience methods out of main require

--- lib/magic.rb
+++ lib/magic.rb
@@ -2,19 +2,5 @@
 require 'magic/flags'
 require 'magic_ffi'
 
-class Magic
-  # A convenience method for compiling files using a Magic object
-  # which is created and closed just for that purpose.
-  #
-  # Arguments are passed directly to Magic.compile
-  def self.compile(*args)
-    m=new()
-    begin
-      return m.compile(*args)
-    ensure
-      m.close()
-    end
-  end
+require 'magic/convenience'
 
-
-end
---
+++ lib/magic/convenience.rb
@@ -0,0 +1,15 @@
+
+class Magic
+  # A convenience method for compiling files using a Magic object
+  # which is created and closed just for that purpose.
+  #
+  # Arguments are passed directly to Magic.compile
+  def self.compile(*args)
+    m=new()
+    begin
+      return m.compile(*args)
+    ensure
+      m.close()
+    end
+  end
+end
