c2738d7a38ed67d504330286d19204c6a0b1100b
Eric Monti <esmonti@gmail.com>
2011-02-19 13:56:30 -0600

    add spec for flag values

--- spec/magic_ffi_spec.rb
+++ spec/magic_ffi_spec.rb
@@ -11,4 +11,20 @@ if Magic != MagicFFI
 
     it_should_behave_like "Magic compiling interface"
   end
+
+  describe MagicFFI::Flags do
+    context "Checking Values" do
+      Magic::Flags.constants.each do |const|
+        it "should have the correct value for #{const}" do
+          Magic::Flags.const_get(const).should == MagicFFI::Flags.const_get(const)
+        end
+      end
+    end
+  end
+else
+  describe "C Binding" do
+    it "was not compiled" do
+      pending "unable to test C bindings"
+    end
+  end
 end
