03f9aed9bb6fec1f84186745d75542f19c3e90a7
Eric Monti <esmonti@gmail.com>
2011-02-20 10:29:56 -0600

    add file and string singleton methods and doc

--- lib/magic/convenience.rb
+++ lib/magic/convenience.rb
@@ -1,26 +1,78 @@
 
 module MagicHelpers
-  # A convenience method for checking syntax files using a Magic object
-  # which is created and closed just for that purpose.
+  # A convenience method for checking syntax of magicdb files.
   #
-  # Arguments are passed directly to Magic.check_syntax
-  def check_syntax(*args)
-    m=new()
+  # Note: syntax errors and warnings may be displayed on stderr.
+  #
+  # @param String fname
+  #   Filename or directory to compile
+  #
+  # @param Hash params
+  #   A hash of parameters to Magic.new()
+  #
+  # @return true,false
+  def check_syntax(fname, params={})
+    m=new(params)
     begin
-      return m.check_syntax(*args)
+      return m.check_syntax(fname)
     ensure
       m.close()
     end
   end
 
-  # A convenience method for compiling files using a Magic object
-  # which is created and closed just for that purpose.
+  # A convenience method for compiling magicdb files.
   #
-  # Arguments are passed directly to Magic.compile
-  def compile(*args)
-    m=new()
+  # @param String fname
+  #   Filename or directory to compile
+  #
+  # @param Hash params
+  #   A hash of parameters to Magic.new()
+  #
+  # @return true
+  #
+  # @raise Magic::CompileError if an error occurs
+  def compile(fname, params={})
+    m=new(params)
     begin
-      return m.compile(*args)
+      return m.compile(fname)
+    ensure
+      m.close()
+    end
+  end
+
+  # A convenience method for identifying file contents
+  #
+  # @param String fname
+  #   Filename to identify
+  #
+  # @param Hash params
+  #   A hash of parameters to Magic.new()
+  #
+  # @return String
+  #   Identification of the file contents.
+  def file(fname, params={})
+    m=new(params)
+    begin
+      return m.file(fname)
+    ensure
+      m.close()
+    end
+  end
+
+  # A convenience method for identifying string contents
+  #
+  # @param String buf
+  #   String contents to identify
+  #
+  # @param Hash params
+  #   A hash of parameters to Magic.new()
+  #
+  # @return String
+  #   Identification of the string.
+  def string(buf, params={})
+    m=new(params)
+    begin
+      return m.string(buf)
     ensure
       m.close()
     end
